ce550be5cf063a9c6b4f7c77d2a815be016d89f1
Luis Capiral <ihategmpls@gmail.com>
2026-03-22 21:24:52 +0800

    refactor: github link to ruicchi link

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,7 +70,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/ruicchi" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>© 2026 Luis Capiral</span>
             <span>Built with Motion-Primitives.</span>
